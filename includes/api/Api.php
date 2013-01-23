wikimedia/mediawiki-extensions-Wikibase

Make use of new sites code in core
Change-Id: Ie639247b59abce24c9c43420de8a642e63f0978b

repo/includes/api/Api.php
@@ -346,8 +346,7 @@ abstract class Api extends \ApiBase {
 	 * @return \SiteList
 	 */
 	protected function getSiteLinkTargetSites() {
-		$group = Settings::get( 'siteLinkGroup' );
-		return \Sites::singleton()->getSiteGroup( $group );
+		return \SitesTable::newInstance()->getSites()->getGroup( Settings::get( 'siteLinkGroup' ) );
 	}
 
 

repo/includes/specials/SpecialCreateItem.php
@@ -72,8 +72,9 @@ class SpecialCreateItem extends SpecialCreateEntity {
 		$status = parent::modifyEntity( $itemContent );
 
 		if ( $this->site !== null && $this->page !== null ) {
-			$site = \Sites::singleton()->getSite( $this->site );
-			if ( $site === false ) {
+			$site = \SitesTable::newInstance()->getSite( $this->site );
+
+			if ( $site === null ) {
 				$status->error( 'wikibase-createitem-not-recognized-siteid' );
 				return $status;
 			}

repo/includes/specials/SpecialItemByTitle.php
@@ -98,7 +98,7 @@ class SpecialItemByTitle extends SpecialItemResolver {
 	protected function switchForm( $siteId, $page ) {
 
 		$group = \Wikibase\Settings::get( 'siteLinkGroup' );
-		$sites = \Sites::singleton()->getSiteGroup( $group );
+		$sites = \SitesTable::newInstance()->getSites()->getGroup( $group );
 
 		$siteExists = $sites->hasSite( $siteId );
 

wmde/WikibaseRepository

includes/api/Api.php
@@ -346,8 +346,7 @@ abstract class Api extends \ApiBase {
 	 * @return \SiteList
 	 */
 	protected function getSiteLinkTargetSites() {
-		$group = Settings::get( 'siteLinkGroup' );
-		return \Sites::singleton()->getSiteGroup( $group );
+		return \SiteSQLStore::newInstance()->getSites()->getGroup( Settings::get( 'siteLinkGroup' ) );
 	}
 
 

includes/specials/SpecialCreateItem.php
@@ -79,8 +79,9 @@ class SpecialCreateItem extends SpecialCreateEntity {
 		$status = parent::modifyEntity( $itemContent );
 
 		if ( $this->site !== null && $this->page !== null ) {
-			$site = \Sites::singleton()->getSite( $this->site );
-			if ( $site === false ) {
+			$site = \SiteSQLStore::newInstance()->getSite( $this->site );
+
+			if ( $site === null ) {
 				$status->error( 'wikibase-createitem-not-recognized-siteid' );
 				return $status;
 			}

includes/specials/SpecialItemByTitle.php
@@ -98,7 +98,7 @@ class SpecialItemByTitle extends SpecialItemResolver {
 	protected function switchForm( $siteId, $page ) {
 
 		$group = \Wikibase\Settings::get( 'siteLinkGroup' );
-		$sites = \Sites::singleton()->getSiteGroup( $group );
+		$sites = \SiteSQLStore::newInstance()->getSites()->getGroup( $group );
 
 		$siteExists = $sites->hasSite( $siteId );
 