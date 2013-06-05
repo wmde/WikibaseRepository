wikimedia/mediawiki-extensions-Wikibase

Split EntityPerPage rebuilder code into separate class
Change-Id: I1325ffb20d4094890337084cd2379652e0099c4e

repo/Wikibase.classes.php
@@ -123,6 +123,7 @@ return call_user_func( function() {
 		// includes/store/sql
 		'Wikibase\SqlIdGenerator' => 'includes/store/sql/SqlIdGenerator.php',
 		'Wikibase\SqlStore' => 'includes/store/sql/SqlStore.php',
+		'Wikibase\EntityPerPageRebuilder' => 'includes/store/sql/EntityPerPageRebuilder.php',
 		'Wikibase\EntityPerPageTable' => 'includes/store/sql/EntityPerPageTable.php',
 		'Wikibase\DispatchStats' => 'includes/store/sql/DispatchStats.php',
 		'Wikibase\TermSearchKeyBuilder' => 'includes/store/sql/TermSearchKeyBuilder.php',

wmde/WikibaseRepository

Wikibase.classes.php
@@ -132,6 +132,7 @@ return call_user_func( function() {
 		// includes/store/sql
 		'Wikibase\SqlIdGenerator' => 'includes/store/sql/SqlIdGenerator.php',
 		'Wikibase\SqlStore' => 'includes/store/sql/SqlStore.php',
+		'Wikibase\EntityPerPageBuilder' => 'includes/store/sql/EntityPerPageBuilder.php',
 		'Wikibase\EntityPerPageTable' => 'includes/store/sql/EntityPerPageTable.php',
 		'Wikibase\DispatchStats' => 'includes/store/sql/DispatchStats.php',
 		'Wikibase\TermSearchKeyBuilder' => 'includes/store/sql/TermSearchKeyBuilder.php',