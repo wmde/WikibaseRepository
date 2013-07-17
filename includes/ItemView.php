wikimedia/mediawiki-extensions-Wikibase

(bug 50798) Allow dynamic site link group section headings in item view
Change-Id: Ibd63ad8b5f1d90628151412deacfe89e7dc94822

repo/includes/ItemView.php
@@ -90,9 +90,10 @@ class ItemView extends EntityView {
 		$html = $thead = $tbody = $tfoot = '';
 
 		$html .= wfTemplate(
-			'wb-section-heading',
+			'wb-section-sitelink-heading',
 			wfMessage( 'wikibase-sitelinks-' . $group )->parse(), // heading
-			htmlspecialchars( 'sitelinks-' . $group, ENT_QUOTES ) // ID
+			htmlspecialchars( 'sitelinks-' . $group, ENT_QUOTES ), // ID
+			'wb-sitelink-heading'
 			// TODO: support entity-id as prefix for element IDs.
 		);
 

wmde/WikibaseRepository

includes/ItemView.php
@@ -90,7 +90,7 @@ class ItemView extends EntityView {
 		$html = $thead = $tbody = $tfoot = '';
 
 		$html .= wfTemplate(
-			'wb-section-heading',
+			'wb-section-heading-sitelinks',
 			wfMessage( 'wikibase-sitelinks-' . $group )->parse(), // heading
 			htmlspecialchars( 'sitelinks-' . $group, ENT_QUOTES ) // ID
 			// TODO: support entity-id as prefix for element IDs.