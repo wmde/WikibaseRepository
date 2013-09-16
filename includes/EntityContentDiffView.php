wikimedia/mediawiki-extensions-Wikibase

(bug 46366) Use SnakFormatter for diffs and summaries
This uses the new SnakFormatterFactory facility to allow
ClaimDifferenceVisualizer and ClaimSummaryBuilder to
operate on snaks in a generic way, without any knowledge about
data types or data values.

Change-Id: Ide1e947d9073f4648c4b97fd979c68c7ff48a984

repo/includes/EntityContentDiffView.php
@@ -5,6 +5,10 @@ use Diff\CallbackListDiffer;
 use Diff\ListDiffer;
 
 use Content, Html;
+use ValueFormatters\FormatterOptions;
+use ValueFormatters\ValueFormatter;
+use Wikibase\Lib\EntityIdLabelFormatter;
+use Wikibase\Lib\SnakFormatterFactory;
 use Wikibase\Repo\WikibaseRepo;
 
 /**
@@ -49,7 +53,14 @@ abstract class EntityContentDiffView extends \DifferenceEngine {
 	public function __construct( $context = null, $old = 0, $new = 0, $rcid = 0, $refreshCache = false, $unhide = false ) {
 		parent::__construct( $context, $old, $new, $rcid, $refreshCache, $unhide );
 
-		$this->mRefreshCache = true; //FIXME: debug only!
+		//TODO: proper injection
+		$options = new FormatterOptions( array(
+			//TODO: fallback chain
+			ValueFormatter::OPT_LANG => $this->getContext()->getLanguage()->getCode()
+		) );
+
+		$this->propertyNameFormatter = new EntityIdLabelFormatter( $options, WikibaseRepo::getDefaultInstance()->getEntityLookup() );
+		$this->snakValueFormatter = WikibaseRepo::getDefaultInstance()->getSnakFormatterFactory()->getFormatter( SnakFormatterFactory::FORMAT_PLAIN, $options );
 	}
 
 	/**
@@ -134,7 +145,6 @@ abstract class EntityContentDiffView extends \DifferenceEngine {
 		 * @var EntityContent $new
 		 */
 		$diff = $old->getEntity()->getDiff( $new->getEntity() );
-		$langCode = $this->getContext()->getLanguage()->getCode();
 
 		$comparer = function( \Comparable $old, \Comparable $new ) {
 			return $old->equals( $new );
@@ -145,9 +155,8 @@ abstract class EntityContentDiffView extends \DifferenceEngine {
 			$this->getContext(),
 			new ClaimDiffer( new CallbackListDiffer( $comparer ) ),
 			new ClaimDifferenceVisualizer(
-				new WikiPageEntityLookup(),
-				$langCode,
-				WikibaseRepo::getDefaultInstance()->getIdFormatter()
+				$this->propertyNameFormatter,
+				$this->snakValueFormatter
 			)
 		);
 

repo/includes/actions/EditEntityAction.php
@@ -3,6 +3,10 @@
 namespace Wikibase;
 use Diff\CallbackListDiffer;
 use  Html, Linker, Skin, Status, Revision;
+use ValueFormatters\FormatterOptions;
+use ValueFormatters\ValueFormatter;
+use Wikibase\Lib\EntityIdLabelFormatter;
+use Wikibase\Lib\SnakFormatterFactory;
 use Wikibase\Repo\WikibaseRepo;
 
 /**
@@ -24,6 +28,21 @@ use Wikibase\Repo\WikibaseRepo;
  * @since 0.1
  */
 abstract class EditEntityAction extends ViewEntityAction {
+
+	public function __construct( \Page $page, \IContextSource $context = null ) {
+		parent::__construct( $page, $context );
+
+		//TODO: proper injection
+		$options = new FormatterOptions( array(
+			//TODO: fallback chain
+			ValueFormatter::OPT_LANG => $this->getContext()->getLanguage()->getCode()
+		) );
+
+		$this->propertyNameFormatter = new EntityIdLabelFormatter( $options, WikibaseRepo::getDefaultInstance()->getEntityLookup() );
+		$this->snakValueFormatter = WikibaseRepo::getDefaultInstance()->getSnakFormatterFactory()->getFormatter( SnakFormatterFactory::FORMAT_PLAIN, $options );
+
+	}
+
 	/**
 	 * @see Action::getName()
 	 *
@@ -440,9 +459,8 @@ abstract class EditEntityAction extends ViewEntityAction {
 			$this->getContext(),
 			new ClaimDiffer( new CallbackListDiffer( $comparer ) ),
 			new ClaimDifferenceVisualizer(
-				new WikiPageEntityLookup(),
-				$langCode,
-				WikibaseRepo::getDefaultInstance()->getIdFormatter()
+				$this->propertyNameFormatter,
+				$this->snakValueFormatter
 			)
 		);
 

repo/includes/api/SetClaim.php
@@ -8,12 +8,15 @@ use Diff\CallbackListDiffer;
 use MWException;
 use ApiBase;
 use Diff\ListDiffer;
+use ValueFormatters\FormatterOptions;
+use ValueFormatters\ValueFormatter;
 use Wikibase\EntityContent;
 use Wikibase\Claim;
 use Wikibase\EntityContentFactory;
 use Wikibase\ClaimDiffer;
 use Wikibase\ClaimSaver;
 use Wikibase\ClaimSummaryBuilder;
+use Wikibase\Lib\SnakFormatterFactory;
 use Wikibase\Repo\WikibaseRepo;
 use Wikibase\Summary;
 use Wikibase\Validators\ValidatorErrorLocalizer;
@@ -85,10 +88,16 @@ class SetClaim extends ApiWikibase {
 		};
 
 		$claimDiffer = new ClaimDiffer( new CallbackListDiffer( $comparer ) );
+
+		$options = new FormatterOptions( array(
+			//TODO: fallback chain
+			ValueFormatter::OPT_LANG => $this->getContext()->getLanguage()->getCode()
+		) );
+
 		$claimSummaryBuilder = new ClaimSummaryBuilder(
 			$this->getModuleName(),
 			$claimDiffer,
-			WikibaseRepo::getDefaultInstance()->getIdFormatter()
+			WikibaseRepo::getDefaultInstance()->getSnakFormatterFactory()->getFormatter( SnakFormatterFactory::FORMAT_PLAIN, $options )
 		);
 		$claimSaver = new ClaimSaver();
 

wmde/WikibaseRepository

includes/EntityContentDiffView.php
@@ -5,6 +5,10 @@ use Diff\CallbackListDiffer;
 use Diff\ListDiffer;
 
 use Content, Html;
+use ValueFormatters\FormatterOptions;
+use ValueFormatters\ValueFormatter;
+use Wikibase\Lib\EntityIdLabelFormatter;
+use Wikibase\Lib\SnakFormatter;
 use Wikibase\Repo\WikibaseRepo;
 
 /**
@@ -49,7 +53,14 @@ abstract class EntityContentDiffView extends \DifferenceEngine {
 	public function __construct( $context = null, $old = 0, $new = 0, $rcid = 0, $refreshCache = false, $unhide = false ) {
 		parent::__construct( $context, $old, $new, $rcid, $refreshCache, $unhide );
 
-		$this->mRefreshCache = true; //FIXME: debug only!
+		//TODO: proper injection
+		$options = new FormatterOptions( array(
+			//TODO: fallback chain
+			ValueFormatter::OPT_LANG => $this->getContext()->getLanguage()->getCode()
+		) );
+
+		$this->propertyNameFormatter = new EntityIdLabelFormatter( $options, WikibaseRepo::getDefaultInstance()->getEntityLookup() );
+		$this->snakValueFormatter = WikibaseRepo::getDefaultInstance()->getSnakFormatterFactory()->getFormatter( SnakFormatter::FORMAT_PLAIN, $options );
 	}
 
 	/**
@@ -134,7 +145,6 @@ abstract class EntityContentDiffView extends \DifferenceEngine {
 		 * @var EntityContent $new
 		 */
 		$diff = $old->getEntity()->getDiff( $new->getEntity() );
-		$langCode = $this->getContext()->getLanguage()->getCode();
 
 		$comparer = function( \Comparable $old, \Comparable $new ) {
 			return $old->equals( $new );
@@ -145,9 +155,8 @@ abstract class EntityContentDiffView extends \DifferenceEngine {
 			$this->getContext(),
 			new ClaimDiffer( new CallbackListDiffer( $comparer ) ),
 			new ClaimDifferenceVisualizer(
-				new WikiPageEntityLookup(),
-				$langCode,
-				WikibaseRepo::getDefaultInstance()->getIdFormatter()
+				$this->propertyNameFormatter,
+				$this->snakValueFormatter
 			)
 		);
 

includes/actions/EditEntityAction.php
@@ -3,6 +3,10 @@
 namespace Wikibase;
 use Diff\CallbackListDiffer;
 use  Html, Linker, Skin, Status, Revision;
+use ValueFormatters\FormatterOptions;
+use ValueFormatters\ValueFormatter;
+use Wikibase\Lib\EntityIdLabelFormatter;
+use Wikibase\Lib\SnakFormatter;
 use Wikibase\Repo\WikibaseRepo;
 
 /**
@@ -24,6 +28,21 @@ use Wikibase\Repo\WikibaseRepo;
  * @since 0.1
  */
 abstract class EditEntityAction extends ViewEntityAction {
+
+	public function __construct( \Page $page, \IContextSource $context = null ) {
+		parent::__construct( $page, $context );
+
+		//TODO: proper injection
+		$options = new FormatterOptions( array(
+			//TODO: fallback chain
+			ValueFormatter::OPT_LANG => $this->getContext()->getLanguage()->getCode()
+		) );
+
+		$this->propertyNameFormatter = new EntityIdLabelFormatter( $options, WikibaseRepo::getDefaultInstance()->getEntityLookup() );
+		$this->snakValueFormatter = WikibaseRepo::getDefaultInstance()->getSnakFormatterFactory()->getFormatter( SnakFormatter::FORMAT_PLAIN, $options );
+
+	}
+
 	/**
 	 * @see Action::getName()
 	 *
@@ -440,9 +459,8 @@ abstract class EditEntityAction extends ViewEntityAction {
 			$this->getContext(),
 			new ClaimDiffer( new CallbackListDiffer( $comparer ) ),
 			new ClaimDifferenceVisualizer(
-				new WikiPageEntityLookup(),
-				$langCode,
-				WikibaseRepo::getDefaultInstance()->getIdFormatter()
+				$this->propertyNameFormatter,
+				$this->snakValueFormatter
 			)
 		);
 

includes/api/SetClaim.php
@@ -8,12 +8,15 @@ use Diff\CallbackListDiffer;
 use MWException;
 use ApiBase;
 use Diff\ListDiffer;
+use ValueFormatters\FormatterOptions;
+use ValueFormatters\ValueFormatter;
 use Wikibase\EntityContent;
 use Wikibase\Claim;
 use Wikibase\EntityContentFactory;
 use Wikibase\ClaimDiffer;
 use Wikibase\ClaimSaver;
 use Wikibase\ClaimSummaryBuilder;
+use Wikibase\Lib\SnakFormatter;
 use Wikibase\Repo\WikibaseRepo;
 use Wikibase\Summary;
 use Wikibase\Validators\ValidatorErrorLocalizer;
@@ -85,10 +88,16 @@ class SetClaim extends ApiWikibase {
 		};
 
 		$claimDiffer = new ClaimDiffer( new CallbackListDiffer( $comparer ) );
+
+		$options = new FormatterOptions( array(
+			//TODO: fallback chain
+			ValueFormatter::OPT_LANG => $this->getContext()->getLanguage()->getCode()
+		) );
+
 		$claimSummaryBuilder = new ClaimSummaryBuilder(
 			$this->getModuleName(),
 			$claimDiffer,
-			WikibaseRepo::getDefaultInstance()->getIdFormatter()
+			WikibaseRepo::getDefaultInstance()->getSnakFormatterFactory()->getFormatter( SnakFormatter::FORMAT_PLAIN, $options )
 		);
 		$claimSaver = new ClaimSaver();
 