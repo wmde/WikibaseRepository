wikimedia/mediawiki-extensions-Wikibase

Replace wb.fetchedEntities with wb.store.EntityStore
Bug: 54082
Change-Id: Ib1622ebd324528154c4eb9f24b8353a7ae66c15b

repo/resources/wikibase.ui.entityViewInit.js
@@ -102,10 +102,14 @@
 			// the entity node (see FIXME below).
 			$claims.toolbarcontroller( toolbarControllerConfig ); // BUILD TOOLBARS
 
+			var entityStore = new wb.store.EntityStore();
+			wb.seedEntityStoreFromMwConfig( entityStore );
+
 			// FIXME: Initializing entityview on $claims leads to the claim section inserted as
 			// child of $claims. It should be direct child of ".wb-entity".
 			$claims.entityview( {
-				value: wb.entity
+				value: wb.entity,
+				entityStore: entityStore
 			} ).appendTo( $claimsParent );
 
 			// add 'wb-claim' id to entity page's Claims heading:

wmde/WikibaseRepository

resources/wikibase.ui.entityViewInit.js
@@ -103,10 +103,14 @@
 			// the entity node (see FIXME below).
 			$claims.toolbarcontroller( toolbarControllerConfig ); // BUILD TOOLBARS
 
+			var entityStore = new wb.store.EntityStore();
+			wb.compileEntityStoreFromMwConfig( entityStore );
+
 			// FIXME: Initializing entityview on $claims leads to the claim section inserted as
 			// child of $claims. It should be direct child of ".wb-entity".
 			$claims.entityview( {
-				value: wb.entity
+				value: wb.entity,
+				entityStore: entityStore
 			} ).appendTo( $claimsParent );
 
 			// This is here to be sure there is never a duplicate id