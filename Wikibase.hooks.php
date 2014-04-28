wikimedia/mediawiki-extensions-Wikibase

(bug 49583) refactoring term validation
Use ValueValidator and EntityValidator to validate terms and
enforce constraints on edit. Introduces TermValidatorFactory
for defining validators based on the entity type.

Change-Id: I3778f7b4d9a212e7c6a83b3b156da8c1b64ccdf0

repo/Wikibase.hooks.php
@@ -1276,7 +1276,7 @@ final class RepoHooks {
 		//       but don't check again that labels aren't be IDs.
 		$validators = array(
 			new LabelUniquenessValidator(
-				$repo->getStore()->getTermIndex()
+				$repo->getTermDuplicateDetector()
 			)
 		);
 

repo/includes/Validators/TermValidatorFactory.php
@@ -1,0 +1,143 @@
+<?php
+
+namespace Wikibase\Validators;
+
+use ValueValidators\ValueValidator;
+use Wikibase\content\EntityValidator;
+use Wikibase\content\LabelDescriptionUniquenessValidator;
+use Wikibase\content\LabelUniquenessValidator;
+use Wikibase\DataModel\Entity\EntityIdParser;
+use Wikibase\DataModel\Entity\Property;
+use Wikibase\TermDuplicateDetector;
+
+
+/**
+ * Provides validators for terms (like the maximum length of labels, etc).
+ *
+ * @since 0.5
+ *
+ * @licence GNU GPL v2+
+ * @author Daniel Kinzler
+ */
+class TermValidatorFactory {
+
+	/**
+	 * @var int
+	 */
+	protected $maxLength;
+
+	/**
+	 * @var string[]
+	 */
+	protected $languages;
+
+	/**
+	 * @var EntityIdParser
+	 */
+	protected $idParser;
+
+	/**
+	 * @param int $maxLength The maximum length of terms.
+	 * @param string[] $languages A list of valid language codes
+	 * @param EntityIdParser $idParser
+	 * @param TermDuplicateDetector $termDuplicateDetector
+	 *
+	 * @throws \InvalidArgumentException
+	 */
+	function __construct(
+		$maxLength,
+		array $languages,
+		EntityIdParser $idParser,
+		TermDuplicateDetector $termDuplicateDetector
+	) {
+		if ( !is_int( $maxLength ) || $maxLength <= 0 ) {
+			throw new \InvalidArgumentException( '$maxLength must be a positive integer.' );
+		}
+
+		$this->maxLength = $maxLength;
+		$this->languages = $languages;
+		$this->idParser = $idParser;
+		$this->termDuplicateDetector = $termDuplicateDetector;
+	}
+
+	/**
+	 * Returns a validator for checking global uniqueness constraints.
+	 * This is intended for checking "soft constraints". For hard constraints,
+	 * see EntityContent::getOnSaveValidators().
+	 *
+	 * @param string $entityType
+	 *
+	 * @return EntityValidator
+	 */
+	public function getUniquenessValidator( $entityType ) {
+		//TODO: Make this configurable. Use a builder. Allow more types to register.
+		if ( $entityType === Property::ENTITY_TYPE ) {
+			return new LabelUniquenessValidator( $this->termDuplicateDetector );
+		} else {
+			return new LabelDescriptionUniquenessValidator( $this->termDuplicateDetector );
+		}
+	}
+
+	/**
+	 * @param string $entityType
+	 *
+	 * @return ValueValidator
+	 */
+	public function getLabelValidator( $entityType ) {
+		$validators = $this->getCommonTermValidators();
+
+		//TODO: Make this configurable. Use a builder. Allow more types to register.
+		if ( $entityType === Property::ENTITY_TYPE ) {
+			$validators[] = new NotEntityIdValidator( $this->idParser, array( Property::ENTITY_TYPE ) );
+		}
+
+		return new CompositeValidator( $validators, true );
+	}
+
+	/**
+	 * @param string $entityType
+	 *
+	 * @return ValueValidator
+	 */
+	public function getDescriptionValidator( $entityType ) {
+		$validators = $this->getCommonTermValidators();
+
+		return new CompositeValidator( $validators, true );
+	}
+
+	/**
+	 * @param string $entityType
+	 *
+	 * @return ValueValidator
+	 */
+	public function getAliasValidator( $entityType ) {
+		$validators = $this->getCommonTermValidators();
+
+		return new CompositeValidator( $validators, true );
+	}
+
+	/**
+	 * @return ValueValidator[]
+	 */
+	protected function getCommonTermValidators() {
+		$validators = array();
+		$validators[] = new TypeValidator( 'string' );
+		$validators[] = new StringLengthValidator( 1, $this->maxLength, 'mb_strlen' );
+		$validators[] = new RegexValidator( '/^\s|[\r\n\t]|\s$/', true ); // no leading/trailing whitespace, no line breaks.
+
+		return $validators;
+	}
+
+	/**
+	 * @return ValueValidator
+	 */
+	public function getLanguageValidator() {
+		$validators = array();
+		$validators[] = new TypeValidator( 'string' );
+		$validators[] = new InArrayValidator( $this->languages, 'not-a-language' );
+
+		$validator = new CompositeValidator( $validators, true ); //Note: each validator is fatal
+		return $validator;
+	}
+
+}

repo/includes/WikibaseRepo.php
@@ -14,9 +14,8 @@ use Wikibase\EntityContentFactory;
 use Wikibase\EntityLookup;
 use Wikibase\i18n\ExceptionLocalizer;
 use Wikibase\i18n\WikibaseExceptionLocalizer;
-use Wikibase\LabelDescriptionDuplicateDetector;
-use Wikibase\Lib\ClaimGuidGenerator;
 use Wikibase\LanguageFallbackChainFactory;
+use Wikibase\Lib\ClaimGuidGenerator;
 use Wikibase\Lib\ClaimGuidValidator;
 use Wikibase\Lib\EntityIdLinkFormatter;
 use Wikibase\Lib\EntityRetrievingDataTypeLookup;
@@ -30,6 +29,7 @@ use Wikibase\Lib\WikibaseDataTypeBuilders;
 use Wikibase\Lib\WikibaseSnakFormatterBuilders;
 use Wikibase\Lib\WikibaseValueFormatterBuilders;
 use Wikibase\ParserOutputJsConfigBuilder;
+use Wikibase\PreSaveChecks;
 use Wikibase\ReferencedEntitiesFinder;
 use Wikibase\Settings;
 use Wikibase\SettingsArray;
@@ -37,7 +37,11 @@ use Wikibase\SnakFactory;
 use Wikibase\StoreFactory;
 use Wikibase\StringNormalizer;
 use Wikibase\SummaryFormatter;
+use Wikibase\TermDuplicateDetector;
+use Wikibase\Utils;
 use Wikibase\Validators\SnakValidator;
+use Wikibase\Validators\TermValidatorFactory;
+use Wikibase\Validators\ValidatorErrorLocalizer;
 
 /**
  * Top level factory for the WikibaseRepo extension.
@@ -331,7 +335,7 @@ class WikibaseRepo {
 	 */
 	public function getChangeOpFactory() {
 		return new ChangeOpFactory(
-			new LabelDescriptionDuplicateDetector( $this->getStore()->getTermIndex() ),
+			$this->getTermDuplicateDetector(),
 			$this->getStore()->newSiteLinkCache(),
 			new ClaimGuidGenerator(),
 			$this->getClaimGuidValidator(),
@@ -562,4 +566,45 @@ class WikibaseRepo {
 		return $this->getEntityContentFactory();
 	}
 
+	/**
+	 * @note: this is a temporary facility, for use until all checks have been moved into CHangeOps.
+	 * @return PreSaveChecks
+	 */
+	public function getPreSaveChecks() {
+		return new PreSaveChecks(
+			$this->getTermValidatorFactory(),
+			$this->getValidatorErrorLocalizer()
+		);
+	}
+
+	/**
+	 * @return TermValidatorFactory
+	 */
+	protected function getTermValidatorFactory() {
+		$constraints = $this->getSettings()->getSetting( 'multilang-limits' );
+		$maxLength = $constraints['length'];
+
+		$languages = Utils::getLanguageCodes();
+
+		return new TermValidatorFactory(
+			$maxLength,
+			$languages,
+			$this->getEntityIdParser(),
+			$this->getTermDuplicateDetector()
+		);
+	}
+
+	/**
+	 * @return ValidatorErrorLocalizer
+	 */
+	public function getValidatorErrorLocalizer() {
+		return new ValidatorErrorLocalizer();
+	}
+
+	/**
+	 * @return TermDuplicateDetector
+	 */
+	public function getTermDuplicateDetector() {
+		return new TermDuplicateDetector( $this->getStore()->getTermIndex() );
+	}
 }

repo/tests/phpunit/includes/Validators/TermValidatorFactoryTest.php
@@ -1,0 +1,191 @@
+<?php
+
+namespace Wikibase\Test;
+
+use ValueValidators\Error;
+use ValueValidators\Result;
+use Wikibase\DataModel\Entity\BasicEntityIdParser;
+use Wikibase\DataModel\Entity\Entity;
+use Wikibase\DataModel\Entity\Item;
+use Wikibase\DataModel\Entity\Property;
+use Wikibase\TermDuplicateDetector;
+use Wikibase\Validators\TermValidatorFactory;
+use Wikibase\SettingsArray;
+
+/**
+ * @covers TermValidatorFactory
+ *
+ * @group WikibaseLib
+ * @group Wikibase
+ * @group WikibaseValidators
+ *
+ * @license GPL 2+
+ * @author Daniel Kinzler
+ */
+class TermValidatorFactoryTest extends \PHPUnit_Framework_TestCase {
+
+	public function detectLabelConflictsForEntity( Entity $entity ) {
+		foreach ( $entity->getLabels() as $lang => $label ) {
+			if ( $label === 'DUPE' ) {
+				return Result::newError( array(
+					Error::newError(
+						'found conflicting terms',
+						'label',
+						'label-conflict',
+						array(
+							'label',
+							$lang,
+							$label,
+							'P666'
+						)
+					)
+				) );
+			}
+		}
+
+		return Result::newSuccess();
+	}
+
+	public function detectLabelDescriptionConflictsForEntity( Entity $entity ) {
+		foreach ( $entity->getLabels() as $lang => $label ) {
+			$description = $entity->getDescription( $lang );
+
+			if ( $description === null ) {
+				continue;
+			}
+
+			if ( $label === 'DUPE' && $description === 'DUPE' ) {
+				return Result::newError( array(
+					Error::newError(
+						'found conflicting terms',
+						'label',
+						'label-with-description-conflict',
+						array(
+							'label',
+							$lang,
+							$label,
+							'Q666'
+						)
+					)
+				) );
+			}
+		}
+
+		return Result::newSuccess();
+	}
+
+	/**
+	 * @return TermDuplicateDetector
+	 */
+	private function getMockDupeDetector() {
+		$dupeDetector = $this->getMockBuilder( 'Wikibase\TermDuplicateDetector' )
+			->disableOriginalConstructor()
+			->getMock();
+
+		$dupeDetector->expects( $this->any() )
+			->method( 'detectLabelConflictsForEntity' )
+			->will( $this->returnCallback( array( $this, 'detectLabelConflictsForEntity' ) ) );
+
+		$dupeDetector->expects( $this->any() )
+			->method( 'detectLabelDescriptionConflictsForEntity' )
+			->will( $this->returnCallback( array( $this, 'detectLabelDescriptionConflictsForEntity' ) ) );
+
+		return $dupeDetector;
+	}
+
+	/**
+	 * @param $maxLength
+	 * @param $languages
+	 *
+	 * @return TermValidatorFactory
+	 */
+	protected function newFactory( $maxLength, $languages ) {
+		$idParser = new BasicEntityIdParser();
+		$dupeDetector = $this->getMockDupeDetector();
+
+		$builders = new TermValidatorFactory( $maxLength, $languages, $idParser, $dupeDetector );
+		return $builders;
+	}
+
+	public function testGetUniquenessValidator() {
+		$builders = $this->newFactory( 20, array( 'ja', 'ru' ) );
+
+		$validator = $builders->getUniquenessValidator( Item::ENTITY_TYPE );
+
+		$this->assertInstanceOf( 'Wikibase\content\EntityValidator', $validator );
+
+		$goodEntity = Item::newEmpty();
+		$goodEntity->setLabel( 'en', 'DUPE' );
+		$goodEntity->setDescription( 'en', 'bla' );
+
+		$badEntity = Item::newEmpty();
+		$badEntity->setLabel( 'en', 'DUPE' );
+		$badEntity->setDescription( 'en', 'DUPE' );
+
+		$this->assertTrue( $validator->validateEntity( $goodEntity )->isValid(), 'isValid(good)' );
+		$this->assertFalse( $validator->validateEntity( $badEntity )->isValid(), 'isValid(bad)' );
+	}
+
+	public function testGetLanguageValidator() {
+		$builders = $this->newFactory( 20, array( 'ja', 'ru' ) );
+
+		$validator = $builders->getLanguageValidator();
+
+		$this->assertInstanceOf( 'ValueValidators\ValueValidator', $validator );
+
+		$this->assertTrue( $validator->validate( 'ja' )->isValid() );
+		$this->assertFalse( $validator->validate( 'xx' )->isValid() );
+	}
+
+	public function testGetLabelValidator() {
+		$builders = $this->newFactory( 8, array( 'en' ) );
+
+		$validator = $builders->getLabelValidator( Item::ENTITY_TYPE );
+
+		$this->assertInstanceOf( 'ValueValidators\ValueValidator', $validator );
+
+		$this->assertTrue( $validator->validate( 'foo' )->isValid() );
+		$this->assertFalse( $validator->validate( '' )->isValid() );
+		$this->assertFalse( $validator->validate( '0123456789' )->isValid() );
+	}
+
+	public function testGetLabelValidator_property() {
+		$builders = $this->newFactory( 8, array( 'en' ) );
+
+		$validator = $builders->getLabelValidator( Property::ENTITY_TYPE );
+
+		$this->assertInstanceOf( 'ValueValidators\ValueValidator', $validator );
+
+		$this->assertTrue( $validator->validate( 'foo' )->isValid() );
+		$this->assertFalse( $validator->validate( '' )->isValid() );
+		$this->assertFalse( $validator->validate( '0123456789' )->isValid() );
+
+		$this->assertFalse( $validator->validate( 'P12' )->isValid() );
+		$this->assertTrue( $validator->validate( 'Q12' )->isValid() );
+	}
+
+	public function testGetDescriptionValidator() {
+		$builders = $this->newFactory( 8, array( 'en' ) );
+
+		$validator = $builders->getDescriptionValidator( Item::ENTITY_TYPE );
+
+		$this->assertInstanceOf( 'ValueValidators\ValueValidator', $validator );
+
+		$this->assertTrue( $validator->validate( 'foo' )->isValid() );
+		$this->assertFalse( $validator->validate( '' )->isValid() );
+		$this->assertFalse( $validator->validate( '0123456789' )->isValid() );
+	}
+
+	public function testGetAliasValidator() {
+		$builders = $this->newFactory( 8, array( 'en' ) );
+
+		$validator = $builders->getAliasValidator( Item::ENTITY_TYPE );
+
+		$this->assertInstanceOf( 'ValueValidators\ValueValidator', $validator );
+
+		$this->assertTrue( $validator->validate( 'foo' )->isValid() );
+		$this->assertFalse( $validator->validate( '' )->isValid() );
+		$this->assertFalse( $validator->validate( '0123456789' )->isValid() );
+	}
+
+}

wmde/WikibaseRepository

Wikibase.hooks.php
@@ -1273,7 +1273,7 @@ final class RepoHooks {
 		//       but don't check again that labels aren't be IDs.
 		$validators = array(
 			new LabelUniquenessValidator(
-				$repo->getStore()->getTermIndex()
+				$repo->getLabelDescriptionDuplicateDetector()
 			)
 		);
 

includes/Validators/TermValidatorFactory.php
@@ -1,0 +1,143 @@
+<?php
+
+namespace Wikibase\Validators;
+
+use ValueValidators\ValueValidator;
+use Wikibase\content\EntityValidator;
+use Wikibase\content\LabelDescriptionUniquenessValidator;
+use Wikibase\content\LabelUniquenessValidator;
+use Wikibase\DataModel\Entity\EntityIdParser;
+use Wikibase\DataModel\Entity\Property;
+use Wikibase\LabelDescriptionDuplicateDetector;
+
+
+/**
+ * Provides validators for terms (like the maximum length of labels, etc).
+ *
+ * @since 0.5
+ *
+ * @licence GNU GPL v2+
+ * @author Daniel Kinzler
+ */
+class TermValidatorFactory {
+
+	/**
+	 * @var int
+	 */
+	protected $maxLength;
+
+	/**
+	 * @var string[]
+	 */
+	protected $languages;
+
+	/**
+	 * @var EntityIdParser
+	 */
+	protected $idParser;
+
+	/**
+	 * @param int $maxLength The maximum length of terms.
+	 * @param string[] $languages A list of valid language codes
+	 * @param EntityIdParser $idParser
+	 * @param LabelDescriptionDuplicateDetector $termDuplicateDetector
+	 *
+	 * @throws \InvalidArgumentException
+	 */
+	function __construct(
+		$maxLength,
+		array $languages,
+		EntityIdParser $idParser,
+		LabelDescriptionDuplicateDetector $termDuplicateDetector
+	) {
+		if ( !is_int( $maxLength ) || $maxLength <= 0 ) {
+			throw new \InvalidArgumentException( '$maxLength must be a positive integer.' );
+		}
+
+		$this->maxLength = $maxLength;
+		$this->languages = $languages;
+		$this->idParser = $idParser;
+		$this->termDuplicateDetector = $termDuplicateDetector;
+	}
+
+	/**
+	 * Returns a validator for checking global uniqueness constraints.
+	 * This is intended for checking "soft constraints". For hard constraints,
+	 * see EntityContent::getOnSaveValidators().
+	 *
+	 * @param string $entityType
+	 *
+	 * @return EntityValidator
+	 */
+	public function getUniquenessValidator( $entityType ) {
+		//TODO: Make this configurable. Use a builder. Allow more types to register.
+		if ( $entityType === Property::ENTITY_TYPE ) {
+			return new LabelUniquenessValidator( $this->termDuplicateDetector );
+		} else {
+			return new LabelDescriptionUniquenessValidator( $this->termDuplicateDetector );
+		}
+	}
+
+	/**
+	 * @param string $entityType
+	 *
+	 * @return ValueValidator
+	 */
+	public function getLabelValidator( $entityType ) {
+		$validators = $this->getCommonTermValidators();
+
+		//TODO: Make this configurable. Use a builder. Allow more types to register.
+		if ( $entityType === Property::ENTITY_TYPE ) {
+			$validators[] = new NotEntityIdValidator( $this->idParser, 'label-no-entityid', array( Property::ENTITY_TYPE ) );
+		}
+
+		return new CompositeValidator( $validators, true );
+	}
+
+	/**
+	 * @param string $entityType
+	 *
+	 * @return ValueValidator
+	 */
+	public function getDescriptionValidator( $entityType ) {
+		$validators = $this->getCommonTermValidators();
+
+		return new CompositeValidator( $validators, true );
+	}
+
+	/**
+	 * @param string $entityType
+	 *
+	 * @return ValueValidator
+	 */
+	public function getAliasValidator( $entityType ) {
+		$validators = $this->getCommonTermValidators();
+
+		return new CompositeValidator( $validators, true );
+	}
+
+	/**
+	 * @return ValueValidator[]
+	 */
+	protected function getCommonTermValidators() {
+		$validators = array();
+		$validators[] = new TypeValidator( 'string' );
+		$validators[] = new StringLengthValidator( 1, $this->maxLength, 'mb_strlen' );
+		$validators[] = new RegexValidator( '/^\s|[\r\n\t]|\s$/', true ); // no leading/trailing whitespace, no line breaks.
+
+		return $validators;
+	}
+
+	/**
+	 * @return ValueValidator
+	 */
+	public function getLanguageValidator() {
+		$validators = array();
+		$validators[] = new TypeValidator( 'string' );
+		$validators[] = new MembershipValidator( $this->languages, 'not-a-language' );
+
+		$validator = new CompositeValidator( $validators, true ); //Note: each validator is fatal
+		return $validator;
+	}
+
+}

includes/WikibaseRepo.php
@@ -14,7 +14,6 @@ use Wikibase\EntityContentFactory;
 use Wikibase\EntityLookup;
 use Wikibase\i18n\ExceptionLocalizer;
 use Wikibase\i18n\WikibaseExceptionLocalizer;
-use Wikibase\LabelDescriptionDuplicateDetector;
 use Wikibase\LanguageFallbackChainFactory;
 use Wikibase\Lib\ClaimGuidGenerator;
 use Wikibase\Lib\ClaimGuidValidator;
@@ -30,6 +29,7 @@ use Wikibase\Lib\WikibaseDataTypeBuilders;
 use Wikibase\Lib\WikibaseSnakFormatterBuilders;
 use Wikibase\Lib\WikibaseValueFormatterBuilders;
 use Wikibase\ParserOutputJsConfigBuilder;
+use Wikibase\PreSaveChecks;
 use Wikibase\ReferencedEntitiesFinder;
 use Wikibase\Settings;
 use Wikibase\SettingsArray;
@@ -37,7 +37,11 @@ use Wikibase\SnakFactory;
 use Wikibase\StoreFactory;
 use Wikibase\StringNormalizer;
 use Wikibase\SummaryFormatter;
+use Wikibase\LabelDescriptionDuplicateDetector;
+use Wikibase\Utils;
 use Wikibase\Validators\SnakValidator;
+use Wikibase\Validators\TermValidatorFactory;
+use Wikibase\Validators\ValidatorErrorLocalizer;
 
 /**
  * Top level factory for the WikibaseRepo extension.
@@ -331,7 +335,7 @@ class WikibaseRepo {
 	 */
 	public function getChangeOpFactory() {
 		return new ChangeOpFactory(
-			new LabelDescriptionDuplicateDetector( $this->getStore()->getTermIndex() ),
+			$this->getLabelDescriptionDuplicateDetector(),
 			$this->getStore()->newSiteLinkCache(),
 			new ClaimGuidGenerator(),
 			$this->getClaimGuidValidator(),
@@ -565,4 +569,45 @@ class WikibaseRepo {
 		return $this->getEntityContentFactory();
 	}
 
+	/**
+	 * @note: this is a temporary facility, for use until all checks have been moved into CHangeOps.
+	 * @return PreSaveChecks
+	 */
+	public function getPreSaveChecks() {
+		return new PreSaveChecks(
+			$this->getTermValidatorFactory(),
+			$this->getValidatorErrorLocalizer()
+		);
+	}
+
+	/**
+	 * @return TermValidatorFactory
+	 */
+	protected function getTermValidatorFactory() {
+		$constraints = $this->getSettings()->getSetting( 'multilang-limits' );
+		$maxLength = $constraints['length'];
+
+		$languages = Utils::getLanguageCodes();
+
+		return new TermValidatorFactory(
+			$maxLength,
+			$languages,
+			$this->getEntityIdParser(),
+			$this->getLabelDescriptionDuplicateDetector()
+		);
+	}
+
+	/**
+	 * @return ValidatorErrorLocalizer
+	 */
+	public function getValidatorErrorLocalizer() {
+		return new ValidatorErrorLocalizer();
+	}
+
+	/**
+	 * @return LabelDescriptionDuplicateDetector
+	 */
+	public function getLabelDescriptionDuplicateDetector() {
+		return new LabelDescriptionDuplicateDetector( $this->getStore()->getTermIndex() );
+	}
 }

tests/phpunit/includes/Validators/TermValidatorFactoryTest.php
@@ -1,0 +1,191 @@
+<?php
+
+namespace Wikibase\Test;
+
+use ValueValidators\Error;
+use ValueValidators\Result;
+use Wikibase\DataModel\Entity\BasicEntityIdParser;
+use Wikibase\DataModel\Entity\Entity;
+use Wikibase\DataModel\Entity\Item;
+use Wikibase\DataModel\Entity\Property;
+use Wikibase\LabelDescriptionDuplicateDetector;
+use Wikibase\Validators\TermValidatorFactory;
+use Wikibase\SettingsArray;
+
+/**
+ * @covers TermValidatorFactory
+ *
+ * @group WikibaseLib
+ * @group Wikibase
+ * @group WikibaseValidators
+ *
+ * @license GPL 2+
+ * @author Daniel Kinzler
+ */
+class TermValidatorFactoryTest extends \PHPUnit_Framework_TestCase {
+
+	public function detectLabelConflictsForEntity( Entity $entity ) {
+		foreach ( $entity->getLabels() as $lang => $label ) {
+			if ( $label === 'DUPE' ) {
+				return Result::newError( array(
+					Error::newError(
+						'found conflicting terms',
+						'label',
+						'label-conflict',
+						array(
+							'label',
+							$lang,
+							$label,
+							'P666'
+						)
+					)
+				) );
+			}
+		}
+
+		return Result::newSuccess();
+	}
+
+	public function detectLabelDescriptionConflictsForEntity( Entity $entity ) {
+		foreach ( $entity->getLabels() as $lang => $label ) {
+			$description = $entity->getDescription( $lang );
+
+			if ( $description === null ) {
+				continue;
+			}
+
+			if ( $label === 'DUPE' && $description === 'DUPE' ) {
+				return Result::newError( array(
+					Error::newError(
+						'found conflicting terms',
+						'label',
+						'label-with-description-conflict',
+						array(
+							'label',
+							$lang,
+							$label,
+							'Q666'
+						)
+					)
+				) );
+			}
+		}
+
+		return Result::newSuccess();
+	}
+
+	/**
+	 * @return LabelDescriptionDuplicateDetector
+	 */
+	private function getMockDupeDetector() {
+		$dupeDetector = $this->getMockBuilder( 'Wikibase\LabelDescriptionDuplicateDetector' )
+			->disableOriginalConstructor()
+			->getMock();
+
+		$dupeDetector->expects( $this->any() )
+			->method( 'detectLabelConflictsForEntity' )
+			->will( $this->returnCallback( array( $this, 'detectLabelConflictsForEntity' ) ) );
+
+		$dupeDetector->expects( $this->any() )
+			->method( 'detectLabelDescriptionConflictsForEntity' )
+			->will( $this->returnCallback( array( $this, 'detectLabelDescriptionConflictsForEntity' ) ) );
+
+		return $dupeDetector;
+	}
+
+	/**
+	 * @param $maxLength
+	 * @param $languages
+	 *
+	 * @return TermValidatorFactory
+	 */
+	protected function newFactory( $maxLength, $languages ) {
+		$idParser = new BasicEntityIdParser();
+		$dupeDetector = $this->getMockDupeDetector();
+
+		$builders = new TermValidatorFactory( $maxLength, $languages, $idParser, $dupeDetector );
+		return $builders;
+	}
+
+	public function testGetUniquenessValidator() {
+		$builders = $this->newFactory( 20, array( 'ja', 'ru' ) );
+
+		$validator = $builders->getUniquenessValidator( Item::ENTITY_TYPE );
+
+		$this->assertInstanceOf( 'Wikibase\content\EntityValidator', $validator );
+
+		$goodEntity = Item::newEmpty();
+		$goodEntity->setLabel( 'en', 'DUPE' );
+		$goodEntity->setDescription( 'en', 'bla' );
+
+		$badEntity = Item::newEmpty();
+		$badEntity->setLabel( 'en', 'DUPE' );
+		$badEntity->setDescription( 'en', 'DUPE' );
+
+		$this->assertTrue( $validator->validateEntity( $goodEntity )->isValid(), 'isValid(good)' );
+		$this->assertFalse( $validator->validateEntity( $badEntity )->isValid(), 'isValid(bad)' );
+	}
+
+	public function testGetLanguageValidator() {
+		$builders = $this->newFactory( 20, array( 'ja', 'ru' ) );
+
+		$validator = $builders->getLanguageValidator();
+
+		$this->assertInstanceOf( 'ValueValidators\ValueValidator', $validator );
+
+		$this->assertTrue( $validator->validate( 'ja' )->isValid() );
+		$this->assertFalse( $validator->validate( 'xx' )->isValid() );
+	}
+
+	public function testGetLabelValidator() {
+		$builders = $this->newFactory( 8, array( 'en' ) );
+
+		$validator = $builders->getLabelValidator( Item::ENTITY_TYPE );
+
+		$this->assertInstanceOf( 'ValueValidators\ValueValidator', $validator );
+
+		$this->assertTrue( $validator->validate( 'foo' )->isValid() );
+		$this->assertFalse( $validator->validate( '' )->isValid() );
+		$this->assertFalse( $validator->validate( '0123456789' )->isValid() );
+	}
+
+	public function testGetLabelValidator_property() {
+		$builders = $this->newFactory( 8, array( 'en' ) );
+
+		$validator = $builders->getLabelValidator( Property::ENTITY_TYPE );
+
+		$this->assertInstanceOf( 'ValueValidators\ValueValidator', $validator );
+
+		$this->assertTrue( $validator->validate( 'foo' )->isValid() );
+		$this->assertFalse( $validator->validate( '' )->isValid() );
+		$this->assertFalse( $validator->validate( '0123456789' )->isValid() );
+
+		$this->assertFalse( $validator->validate( 'P12' )->isValid() );
+		$this->assertTrue( $validator->validate( 'Q12' )->isValid() );
+	}
+
+	public function testGetDescriptionValidator() {
+		$builders = $this->newFactory( 8, array( 'en' ) );
+
+		$validator = $builders->getDescriptionValidator( Item::ENTITY_TYPE );
+
+		$this->assertInstanceOf( 'ValueValidators\ValueValidator', $validator );
+
+		$this->assertTrue( $validator->validate( 'foo' )->isValid() );
+		$this->assertFalse( $validator->validate( '' )->isValid() );
+		$this->assertFalse( $validator->validate( '0123456789' )->isValid() );
+	}
+
+	public function testGetAliasValidator() {
+		$builders = $this->newFactory( 8, array( 'en' ) );
+
+		$validator = $builders->getAliasValidator( Item::ENTITY_TYPE );
+
+		$this->assertInstanceOf( 'ValueValidators\ValueValidator', $validator );
+
+		$this->assertTrue( $validator->validate( 'foo' )->isValid() );
+		$this->assertFalse( $validator->validate( '' )->isValid() );
+		$this->assertFalse( $validator->validate( '0123456789' )->isValid() );
+	}
+
+}