wikimedia/mediawiki-extensions-Wikibase

Fix the paging order used by EntityPerPage.
The paging order needs to be id/type, not type/id, to
make good use of the database indexes. Attempts to
iterate over all page IDs where very slow, this change
should fix that.

Change-Id: I2b8dc080f03499e3818a03c8fb7a9b5b985aed5b

repo/includes/store/sql/EntityPerPageTable.php
@@ -257,6 +257,10 @@ class EntityPerPageTable implements EntityPerPage {
 
 		$dbr = wfGetDB( DB_SLAVE );
 
+		//NOTE: needs to be id/type, not type/id, according to the definition of the relevant
+		//      index in wikibase.sql: wb_entity_per_page (epp_entity_id, epp_entity_type);
+		$oderBy = array( 'epp_entity_id', 'epp_entity_type' );
+
 		if ( $after ) {
 			if ( $entityType === null ) {
 				// Ugly. About time we switch to qualified, string based IDs!
@@ -265,6 +269,10 @@ class EntityPerPageTable implements EntityPerPage {
 						' OR epp_entity_type > ' . $dbr->addQuotes( $after->getEntityType() ) . ' )';
 			} else {
 				$where[] = 'epp_entity_id > ' . $after->getNumericId();
+
+				// NOTE: If the type is fixed, don't use the type in the order;
+				// before changing this, check index usage.
+				$oderBy = array( 'epp_entity_id' );
 			}
 		}
 
@@ -274,7 +282,7 @@ class EntityPerPageTable implements EntityPerPage {
 			$where,
 			__METHOD__,
 			array(
-				'ORDER BY' => array( 'epp_entity_type', 'epp_entity_id' ),
+				'ORDER BY' => $oderBy,
 				'LIMIT' => $limit
 			)
 		);

wmde/WikibaseRepository

includes/store/sql/EntityPerPageTable.php
@@ -3,7 +3,6 @@
 namespace Wikibase;
 
 use InvalidArgumentException;
-use Iterator;
 use Wikibase\DataModel\Entity\BasicEntityIdParser;
 use Wikibase\DataModel\Entity\ItemId;
 
@@ -245,10 +244,16 @@ class EntityPerPageTable implements EntityPerPage {
 	public function listEntities( $entityType, $limit, EntityId $after = null ) {
 		if ( $entityType == null  ) {
 			$where = array();
+			//NOTE: needs to be id/type, not type/id, according to the definition of the relevant
+			//      index in wikibase.sql: wb_entity_per_page (epp_entity_id, epp_entity_type);
+			$orderBy = array( 'epp_entity_id', 'epp_entity_type' );
 		} elseif ( !is_string( $entityType ) ) {
 			throw new InvalidArgumentException( '$entityType must be a string (or null)' );
 		} else {
 			$where = array( 'epp_entity_type' => $entityType );
+			// NOTE: If the type is fixed, don't use the type in the order;
+			// before changing this, check index usage.
+			$orderBy = array( 'epp_entity_id' );
 		}
 
 		if ( !is_int( $limit ) || $limit < 1 ) {
@@ -274,7 +279,7 @@ class EntityPerPageTable implements EntityPerPage {
 			$where,
 			__METHOD__,
 			array(
-				'ORDER BY' => array( 'epp_entity_type', 'epp_entity_id' ),
+				'ORDER BY' => $orderBy,
 				'LIMIT' => $limit
 			)
 		);