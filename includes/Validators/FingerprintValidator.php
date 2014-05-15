wikimedia/mediawiki-extensions-Wikibase

Move term validation into ChangeOps
NOTE: this changes constraint checking for some cases:
* sitelink constraints are now *always* checked
* label/description uniqueness becomes a "soft" constraint,
  checked only for new input.

NOTE: this also changes how sitelink constraints are enforced
in ChangeOpsMerge. This is a side effect of consolidating
the constraint handling.

Bug: 62643
Change-Id: Ibb6ee8439ba3ab0252403eb811aa48d1274d1d58

repo/includes/Validators/FingerprintValidator.php
@@ -1,0 +1,36 @@
+<?php
+
+namespace Wikibase\Validators;
+
+use ValueValidators\Result;
+use Wikibase\DataModel\Entity\EntityId;
+use Wikibase\DataModel\Term\Fingerprint;
+
+/**
+ * Validator interface for validating Entity Fingerprints.
+ *
+ * This is intended particularly for uniqueness checks.
+ *
+ * @since 0.5
+ *
+ * @licence GNU GPL v2+
+ * @author Daniel Kinzler
+ */
+interface FingerprintValidator {
+
+	/**
+	 * Validate the given fingerprint.
+	 *
+	 * @since 0.5
+	 *
+	 * @param Fingerprint $fingerprint
+	 * @param EntityId|null $entityId Context for uniqueness checks: conflicts with this entity
+	 *        are ignored.
+	 * @param string[]|null $languages If given, the validation may be limited to the given languages;
+	 *        This is intended for optimization for the common case of only a single language changing.
+	 *
+	 * @return Result
+	 */
+	public function validateFingerprint( Fingerprint $fingerprint, EntityId $entityId = null, $languages = null );
+
+}

wmde/WikibaseRepository

includes/Validators/FingerprintValidator.php
@@ -1,0 +1,36 @@
+<?php
+
+namespace Wikibase\Validators;
+
+use ValueValidators\Result;
+use Wikibase\DataModel\Entity\EntityId;
+use Wikibase\DataModel\Term\Fingerprint;
+
+/**
+ * Validator interface for validating Entity Fingerprints.
+ *
+ * This is intended particularly for uniqueness checks.
+ *
+ * @since 0.5
+ *
+ * @licence GNU GPL v2+
+ * @author Daniel Kinzler
+ */
+interface FingerprintValidator {
+
+	/**
+	 * Validate the given fingerprint.
+	 *
+	 * @since 0.5
+	 *
+	 * @param Fingerprint $fingerprint
+	 * @param EntityId|null $entityId Context for uniqueness checks: conflicts with this entity
+	 *        are ignored.
+	 * @param string[]|null $languageCodes If given, the validation may be limited to the given languages;
+	 *        This is intended for optimization for the common case of only a single language changing.
+	 *
+	 * @return Result
+	 */
+	public function validateFingerprint( Fingerprint $fingerprint, EntityId $entityId = null, $languageCodes = null );
+
+}