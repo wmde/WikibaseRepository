wikimedia/mediawiki-extensions-Wikibase

Replace ChangeNotifier with ChangeNotificationChannel
This leads up to the introduction of a new/different ChangeNotifier
class that will contain much of the notification related logic
currently in static hook handler functions.

Bug: 66831
Change-Id: If2466a07f6ab6a15e077bba4ff147e3c3ba66953

repo/Wikibase.hooks.php
@@ -249,7 +249,7 @@ final class RepoHooks {
 				'time' => $revision->getTimestamp(),
 			) );
 
-			WikibaseRepo::getDefaultInstance()->newChangeNotifier()->handleChange( $change );
+			WikibaseRepo::getDefaultInstance()->getChangeNotificationChannel()->sendChangeNotification( $change );
 		}
 
 		wfProfileOut( __METHOD__ );
@@ -306,7 +306,7 @@ final class RepoHooks {
 
 		$change->setMetadataFromUser( $user );
 
-		WikibaseRepo::getDefaultInstance()->newChangeNotifier()->handleChange( $change );
+		WikibaseRepo::getDefaultInstance()->getChangeNotificationChannel()->sendChangeNotification( $change );
 
 		wfProfileOut( __METHOD__ );
 		return true;
@@ -368,7 +368,7 @@ final class RepoHooks {
 		$user = User::newFromId( $userId );
 		$change->setMetadataFromUser( $user );
 
-		WikibaseRepo::getDefaultInstance()->newChangeNotifier()->handleChange( $change );
+		WikibaseRepo::getDefaultInstance()->getChangeNotificationChannel()->sendChangeNotification( $change );
 
 		wfProfileOut( __METHOD__ );
 		return true;

repo/tests/phpunit/includes/WikibaseRepoTest.php
@@ -138,6 +138,11 @@ class WikibaseRepoTest extends \MediaWikiTestCase {
 		$this->assertInstanceOf( 'Wikibase\ChangeOp\ChangeOpFactoryProvider', $returnValue );
 	}
 
+	public function testGetChangeNotificationChannel() {
+		$returnValue = $this->getDefaultInstance()->getChangeNotificationChannel();
+		$this->assertInstanceOf( 'Wikibase\Repo\Notifications\ChangeNotificationChannel', $returnValue );
+	}
+
 	public function testGetContentModelMappings() {
 		$array = $this->getDefaultInstance()->getContentModelMappings();
 		foreach( $array as $entityType => $contentModel ) {

wmde/WikibaseRepository

Wikibase.hooks.php
@@ -249,7 +249,7 @@ final class RepoHooks {
 				'time' => $revision->getTimestamp(),
 			) );
 
-			WikibaseRepo::getDefaultInstance()->newChangeNotifier()->handleChange( $change );
+			WikibaseRepo::getDefaultInstance()->getChangeTransmitter()->transmitChange( $change );
 		}
 
 		wfProfileOut( __METHOD__ );
@@ -306,7 +306,7 @@ final class RepoHooks {
 
 		$change->setMetadataFromUser( $user );
 
-		WikibaseRepo::getDefaultInstance()->newChangeNotifier()->handleChange( $change );
+		WikibaseRepo::getDefaultInstance()->getChangeTransmitter()->transmitChange( $change );
 
 		wfProfileOut( __METHOD__ );
 		return true;
@@ -368,7 +368,7 @@ final class RepoHooks {
 		$user = User::newFromId( $userId );
 		$change->setMetadataFromUser( $user );
 
-		WikibaseRepo::getDefaultInstance()->newChangeNotifier()->handleChange( $change );
+		WikibaseRepo::getDefaultInstance()->getChangeTransmitter()->transmitChange( $change );
 
 		wfProfileOut( __METHOD__ );
 		return true;

tests/phpunit/includes/WikibaseRepoTest.php
@@ -138,6 +138,11 @@ class WikibaseRepoTest extends \MediaWikiTestCase {
 		$this->assertInstanceOf( 'Wikibase\ChangeOp\ChangeOpFactoryProvider', $returnValue );
 	}
 
+	public function testGetChangeNotificationChannel() {
+		$returnValue = $this->getDefaultInstance()->getChangeTransmitter();
+		$this->assertInstanceOf( 'Wikibase\Repo\Notifications\ChangeTransmitter', $returnValue );
+	}
+
 	public function testGetContentModelMappings() {
 		$array = $this->getDefaultInstance()->getContentModelMappings();
 		foreach( $array as $entityType => $contentModel ) {