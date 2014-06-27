wikimedia/mediawiki-extensions-Wikibase

Test diffs for redirects
Bug: 65585
Change-Id: Ie4214ca9ef868aa8e9e32ed311c6740a2959fcf8

repo/tests/phpunit/includes/EntityContentDiffViewTest.php
@@ -5,10 +5,12 @@ namespace Wikibase\Test;
 use DerivativeContext;
 use Language;
 use RequestContext;
+use Title;
 use Wikibase\DataModel\Entity\ItemId;
 use Wikibase\EntityContentDiffView;
 use Wikibase\Item;
 use Wikibase\ItemContent;
+use Wikibase\Lib\Store\EntityRedirect;
 
 /**
  * @covers Wikibase\EntityContentDiffView
@@ -44,6 +46,9 @@ class EntityContentDiffViewTest extends \MediaWikiTestCase {
 		$item2->setLabel( 'en', 'O_o' );
 		$item2->removeDescription( 'en' );
 
+		$redirect = new EntityRedirect( new ItemId( 'Q11' ), new ItemId( 'Q21' ) );
+		$redirect2 = new EntityRedirect( new ItemId( 'Q11' ), new ItemId( 'Q22' ) );
+
 		$insTags = array(
 			'has <td>label / de</td>' => array( 'tag' => 'td', 'content' => 'label / de' ),
 			'has <ins>foo</ins>' => array( 'tag' => 'ins', 'content' => 'foo' ),
@@ -75,28 +80,57 @@ class EntityContentDiffViewTest extends \MediaWikiTestCase {
 			'has <del>ohi there</del>' => array( 'tag' => 'del', 'content' => 'ohi there' ),
 		);
 
+		$fromRedirTags = array(
+			'has <td>label / de</td>' => array( 'tag' => 'td', 'content' => 'label / de' ),
+			'has <ins>foo</ins>' => array( 'tag' => 'ins', 'content' => 'foo' ),
+
+			'has <td>redirect</td>' => array( 'tag' => 'td', 'content' => 'redirect' ),
+			'has <del>Q21</del>' => array( 'tag' => 'del', 'content' => 'Q21' ),
+		);
+
+		$toRedirTags = array(
+			'has <td>label / de</td>' => array( 'tag' => 'td', 'content' => 'label / de' ),
+			'has <del>foo</del>' => array( 'tag' => 'del', 'content' => 'foo' ),
+
+			'has <td>redirect</td>' => array( 'tag' => 'td', 'content' => 'redirect' ),
+			'has <ins>Q21</ins>' => array( 'tag' => 'ins', 'content' => 'Q21' ),
+		);
+
+		$changeRedirTags = array(
+			'has <td>redirect</td>' => array( 'tag' => 'td', 'content' => 'redirect' ),
+			'has <del>Q21</del>' => array( 'tag' => 'del', 'content' => 'Q21' ),
+			'has <ins>Q22</del>' => array( 'tag' => 'ins', 'content' => 'Q22' ),
+		);
+
+		$emptyContent = ItemContent::newFromItem( $empty );
+		$itemContent = ItemContent::newFromItem( $item );
+		$item2Content = ItemContent::newFromItem( $item2 );
+
+		$redirectContent = ItemContent::newFromRedirect( $redirect, Title::newFromText( $redirect->getEntityId()->getSerialization() ) );
+		$redirect2Content = ItemContent::newFromRedirect( $redirect2, Title::newFromText( $redirect2->getEntityId()->getSerialization() ) );
+
 		return array(
-			'empty' => array( $empty, $empty, array( 'empty' => '/^$/', ) ),
-			'same' => array( $item, $item, array( 'empty' => '/^$/', ) ),
-			'from emtpy' => array( $empty, $item, $insTags ),
-			'to empty' => array( $item, $empty, $delTags ),
-			'changed' => array( $item, $item2, $changeTags ),
+			'empty' => array( $emptyContent, $emptyContent, array( 'empty' => '/^$/', ) ),
+			'same' => array( $itemContent, $itemContent, array( 'empty' => '/^$/', ) ),
+			'from emtpy' => array( $emptyContent, $itemContent, $insTags ),
+			'to empty' => array( $itemContent, $emptyContent, $delTags ),
+			'changed' => array( $itemContent, $item2Content, $changeTags ),
+			'to redirect' => array( $itemContent, $redirectContent, $toRedirTags ),
+			'from redirect' => array( $redirectContent, $itemContent, $fromRedirTags ),
+			'redirect changed' => array( $redirectContent, $redirect2Content, $changeRedirTags ),
 		);
 	}
 
 	/**
 	 * @dataProvider itemProvider
 	 */
-	public function testGenerateContentDiffBody( Item $item0, Item $item1, $matchers ) {
+	public function testGenerateContentDiffBody( ItemContent $item0, ItemContent $item1, $matchers ) {
 		$context = new DerivativeContext( RequestContext::getMain() );
 		$context->setLanguage( Language::factory( 'en' ) );
 
 		$diffView = new EntityContentDiffView( $context );
 
-		$html = $diffView->generateContentDiffBody(
-			ItemContent::newFromItem( $item0 ),
-			ItemContent::newFromItem( $item1 )
-		);
+		$html = $diffView->generateContentDiffBody( $item0, $item1 );
 
 		$this->assertInternalType( 'string', $html );
 

wmde/WikibaseRepository

tests/phpunit/includes/EntityContentDiffViewTest.php
@@ -5,10 +5,12 @@ namespace Wikibase\Test;
 use DerivativeContext;
 use Language;
 use RequestContext;
+use Title;
+use Wikibase\DataModel\Entity\Item;
 use Wikibase\DataModel\Entity\ItemId;
 use Wikibase\EntityContentDiffView;
-use Wikibase\Item;
 use Wikibase\ItemContent;
+use Wikibase\Lib\Store\EntityRedirect;
 
 /**
  * @covers Wikibase\EntityContentDiffView
@@ -29,8 +31,8 @@ class EntityContentDiffViewTest extends \MediaWikiTestCase {
 	}
 
 	public function itemProvider() {
-		$empty = Item::newEmpty();
-		$empty->setId( new ItemId( 'Q1' ) );
+		$emptyItem = Item::newEmpty();
+		$emptyItem->setId( new ItemId( 'Q1' ) );
 
 		$item = Item::newEmpty();
 		$item->setId( new ItemId( 'Q11' ) );
@@ -43,6 +45,9 @@ class EntityContentDiffViewTest extends \MediaWikiTestCase {
 		$item2->setAliases( 'nl', array( 'daaaah' ) );
 		$item2->setLabel( 'en', 'O_o' );
 		$item2->removeDescription( 'en' );
+
+		$redirect = new EntityRedirect( new ItemId( 'Q11' ), new ItemId( 'Q21' ) );
+		$redirect2 = new EntityRedirect( new ItemId( 'Q11' ), new ItemId( 'Q22' ) );
 
 		$insTags = array(
 			'has <td>label / de</td>' => array( 'tag' => 'td', 'content' => 'label / de' ),
@@ -75,28 +80,63 @@ class EntityContentDiffViewTest extends \MediaWikiTestCase {
 			'has <del>ohi there</del>' => array( 'tag' => 'del', 'content' => 'ohi there' ),
 		);
 
+		$fromRedirTags = array(
+			'has <td>label / de</td>' => array( 'tag' => 'td', 'content' => 'label / de' ),
+			'has <ins>foo</ins>' => array( 'tag' => 'ins', 'content' => 'foo' ),
+
+			'has <td>redirect</td>' => array( 'tag' => 'td', 'content' => 'redirect' ),
+			'has <del>Q21</del>' => array( 'tag' => 'del', 'content' => 'Q21' ),
+		);
+
+		$toRedirTags = array(
+			'has <td>label / de</td>' => array( 'tag' => 'td', 'content' => 'label / de' ),
+			'has <del>foo</del>' => array( 'tag' => 'del', 'content' => 'foo' ),
+
+			'has <td>redirect</td>' => array( 'tag' => 'td', 'content' => 'redirect' ),
+			'has <ins>Q21</ins>' => array( 'tag' => 'ins', 'content' => 'Q21' ),
+		);
+
+		$changeRedirTags = array(
+			'has <td>redirect</td>' => array( 'tag' => 'td', 'content' => 'redirect' ),
+			'has <del>Q21</del>' => array( 'tag' => 'del', 'content' => 'Q21' ),
+			'has <ins>Q22</del>' => array( 'tag' => 'ins', 'content' => 'Q22' ),
+		);
+
+		$empty = ItemContent::newFromItem( $emptyItem );
+		$itemContent = ItemContent::newFromItem( $item );
+		$itemContent2 = ItemContent::newFromItem( $item2 );
+
+		$redirectContent = ItemContent::newFromRedirect(
+			$redirect,
+			Title::newFromText( $redirect->getEntityId()->getSerialization() )
+		);
+		$redirectContent2 = ItemContent::newFromRedirect(
+			$redirect2,
+			Title::newFromText( $redirect2->getEntityId()->getSerialization() )
+		);
+
 		return array(
 			'empty' => array( $empty, $empty, array( 'empty' => '/^$/', ) ),
-			'same' => array( $item, $item, array( 'empty' => '/^$/', ) ),
-			'from emtpy' => array( $empty, $item, $insTags ),
-			'to empty' => array( $item, $empty, $delTags ),
-			'changed' => array( $item, $item2, $changeTags ),
+			'same' => array( $itemContent, $itemContent, array( 'empty' => '/^$/', ) ),
+			'from emtpy' => array( $empty, $itemContent, $insTags ),
+			'to empty' => array( $itemContent, $empty, $delTags ),
+			'changed' => array( $itemContent, $itemContent2, $changeTags ),
+			'to redirect' => array( $itemContent, $redirectContent, $toRedirTags ),
+			'from redirect' => array( $redirectContent, $itemContent, $fromRedirTags ),
+			'redirect changed' => array( $redirectContent, $redirectContent2, $changeRedirTags ),
 		);
 	}
 
 	/**
 	 * @dataProvider itemProvider
 	 */
-	public function testGenerateContentDiffBody( Item $item0, Item $item1, $matchers ) {
+	public function testGenerateContentDiffBody( ItemContent $itemContent, ItemContent $itemContent2, $matchers ) {
 		$context = new DerivativeContext( RequestContext::getMain() );
 		$context->setLanguage( Language::factory( 'en' ) );
 
 		$diffView = new EntityContentDiffView( $context );
 
-		$html = $diffView->generateContentDiffBody(
-			ItemContent::newFromItem( $item0 ),
-			ItemContent::newFromItem( $item1 )
-		);
+		$html = $diffView->generateContentDiffBody( $itemContent, $itemContent2 );
 
 		$this->assertInternalType( 'string', $html );
 