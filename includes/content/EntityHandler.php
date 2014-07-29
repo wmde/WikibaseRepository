wikimedia/mediawiki-extensions-Wikibase

Find better name for DataUpdateClosure
See discussion in
https://gerrit.wikimedia.org/r/#/c/135777/9/repo/includes/updates/DataUpdateClosure.php

Possible names that describe what it is:
CustomDataUpdate
DelegatedDataUpdate

Names that describe what it does:
CustomizedDataUpdate
DelegatingDataUpdate

The question is: Is one of this names really better than the current one?

Change-Id: Ia8048d45a3f30d2dc02c4f884975c54f28d92d25

repo/includes/content/EntityHandler.php
@@ -16,7 +16,7 @@ use Title;
 use User;
 use Wikibase\DataModel\Entity\BasicEntityIdParser;
 use Wikibase\Lib\Store\EntityContentDataCodec;
-use Wikibase\Updates\DataUpdateClosure;
+use Wikibase\Updates\DelegatingDataUpdate;
 use Wikibase\Validators\EntityValidator;
 
 /**
@@ -456,19 +456,19 @@ abstract class EntityHandler extends ContentHandler {
 
 		// Call the WikibaseEntityDeletionUpdate hook.
 		// Do this before doing any well-known updates.
-		$updates[] = new DataUpdateClosure(
+		$updates[] = new DelegatingDataUpdate(
 			'wfRunHooks',
 			'WikibaseEntityDeletionUpdate',
 			array( $content, $title ) );
 
 		// Unregister the entity from the terms table.
-		$updates[] = new DataUpdateClosure(
+		$updates[] = new DelegatingDataUpdate(
 			array( $this->termIndex, 'deleteTermsOfEntity' ),
 			$content->getEntity()->getId()
 		);
 
 		// Unregister the entity from the EntityPerPage table.
-		$updates[] = new DataUpdateClosure(
+		$updates[] = new DelegatingDataUpdate(
 			array( $this->entityPerPage, 'deleteEntityPage' ),
 			$content->getEntity()->getId(),
 			$title->getArticleID()
@@ -496,21 +496,21 @@ abstract class EntityHandler extends ContentHandler {
 		// @todo: Only do this if the entity is new.
 		// Note that $title->exists() will already return true at this point
 		// even if we are just now creating the entity.
-		$updates[] = new DataUpdateClosure(
+		$updates[] = new DelegatingDataUpdate(
 			array( $this->entityPerPage, 'addEntityPage' ),
 			$content->getEntity()->getId(),
 			$title->getArticleID()
 		);
 
 		// Register the entity in the terms table.
-		$updates[] = new DataUpdateClosure(
+		$updates[] = new DelegatingDataUpdate(
 			array( $this->termIndex, 'saveTermsOfEntity' ),
 			$content->getEntity()
 		);
 
 		// Call the WikibaseEntityModificationUpdate hook.
 		// Do this after doing all well-known updates.
-		$updates[] = new DataUpdateClosure(
+		$updates[] = new DelegatingDataUpdate(
 			'wfRunHooks',
 			'WikibaseEntityModificationUpdate',
 			array( $content, $title )

repo/includes/content/ItemHandler.php
@@ -5,7 +5,7 @@ namespace Wikibase;
 use DataUpdate;
 use Title;
 use Wikibase\Lib\Store\EntityContentDataCodec;
-use Wikibase\Updates\DataUpdateClosure;
+use Wikibase\Updates\DelegatingDataUpdate;
 use Wikibase\Validators\EntityValidator;
 
 /**
@@ -105,7 +105,7 @@ class ItemHandler extends EntityHandler {
 	public function getEntityDeletionUpdates( EntityContent $content, Title $title ) {
 		$updates = array();
 
-		$updates[] = new DataUpdateClosure(
+		$updates[] = new DelegatingDataUpdate(
 			array( $this->siteLinkStore, 'deleteLinksOfItem' ),
 			$content->getEntity()->getId()
 		);
@@ -131,7 +131,7 @@ class ItemHandler extends EntityHandler {
 	public function getEntityModificationUpdates( EntityContent $content, Title $title ) {
 		$updates = array();
 
-		$updates[] = new DataUpdateClosure(
+		$updates[] = new DelegatingDataUpdate(
 			array( $this->siteLinkStore, 'saveLinksOfItem' ),
 			$content->getEntity()
 		);

repo/includes/content/PropertyHandler.php
@@ -5,7 +5,7 @@ namespace Wikibase;
 use DataUpdate;
 use Title;
 use Wikibase\Lib\Store\EntityContentDataCodec;
-use Wikibase\Updates\DataUpdateClosure;
+use Wikibase\Updates\DelegatingDataUpdate;
 use Wikibase\Validators\EntityValidator;
 
 /**
@@ -107,7 +107,7 @@ class PropertyHandler extends EntityHandler {
 	public function getEntityDeletionUpdates( EntityContent $content, Title $title ) {
 		$updates = array();
 
-		$updates[] = new DataUpdateClosure(
+		$updates[] = new DelegatingDataUpdate(
 			array( $this->infoStore, 'removePropertyInfo' ),
 			$content->getEntity()->getId()
 		);
@@ -139,7 +139,7 @@ class PropertyHandler extends EntityHandler {
 			PropertyInfoStore::KEY_DATA_TYPE => $content->getProperty()->getDataTypeId()
 		);
 
-		$updates[] = new DataUpdateClosure(
+		$updates[] = new DelegatingDataUpdate(
 			array( $this->infoStore, 'setPropertyInfo' ),
 			$content->getEntity()->getId(),
 			$info

wmde/WikibaseRepository

includes/content/EntityHandler.php
@@ -21,7 +21,7 @@ use ValueValidators\Result;
 use Wikibase\DataModel\Entity\BasicEntityIdParser;
 use Wikibase\Lib\Store\EntityContentDataCodec;
 use Wikibase\Lib\Store\EntityRedirect;
-use Wikibase\Updates\DataUpdateClosure;
+use Wikibase\Updates\DataUpdateAdapter;
 use Wikibase\Validators\EntityValidator;
 use Wikibase\Validators\ValidatorErrorLocalizer;
 
@@ -602,20 +602,20 @@ abstract class EntityHandler extends ContentHandler {
 
 		// Call the WikibaseEntityDeletionUpdate hook.
 		// Do this before doing any well-known updates.
-		$updates[] = new DataUpdateClosure(
+		$updates[] = new DataUpdateAdapter(
 			'wfRunHooks',
 			'WikibaseEntityDeletionUpdate',
 			array( $content, $title )
 		);
 
 		// Unregister the entity from the terms table.
-		$updates[] = new DataUpdateClosure(
+		$updates[] = new DataUpdateAdapter(
 			array( $this->termIndex, 'deleteTermsOfEntity' ),
 			$entityId
 		);
 
 		// Unregister the entity from the EntityPerPage table.
-		$updates[] = new DataUpdateClosure(
+		$updates[] = new DataUpdateAdapter(
 			array( $this->entityPerPage, 'deleteEntityPage' ),
 			$entityId,
 			$title->getArticleID()
@@ -648,13 +648,13 @@ abstract class EntityHandler extends ContentHandler {
 
 		if ( $content->isRedirect() ) {
 			// Remove the entity from the terms table since it's now a redirect.
-			$updates[] = new DataUpdateClosure(
+			$updates[] = new DataUpdateAdapter(
 				array( $this->termIndex, 'deleteTermsOfEntity' ),
 				$entityId
 			);
 
 			// Register the redirect from the EntityPerPage table.
-			$updates[] = new DataUpdateClosure(
+			$updates[] = new DataUpdateAdapter(
 				array( $this->entityPerPage, 'addRedirectPage' ),
 				$entityId,
 				$title->getArticleID(),
@@ -662,14 +662,14 @@ abstract class EntityHandler extends ContentHandler {
 			);
 		} else {
 			// Register the entity in the EntityPerPage table.
-			$updates[] = new DataUpdateClosure(
+			$updates[] = new DataUpdateAdapter(
 				array( $this->entityPerPage, 'addEntityPage' ),
 				$entityId,
 				$title->getArticleID()
 			);
 
 			// Register the entity in the terms table.
-			$updates[] = new DataUpdateClosure(
+			$updates[] = new DataUpdateAdapter(
 				array( $this->termIndex, 'saveTermsOfEntity' ),
 				$content->getEntity()
 			);
@@ -677,7 +677,7 @@ abstract class EntityHandler extends ContentHandler {
 
 		// Call the WikibaseEntityModificationUpdate hook.
 		// Do this after doing all well-known updates.
-		$updates[] = new DataUpdateClosure(
+		$updates[] = new DataUpdateAdapter(
 			'wfRunHooks',
 			'WikibaseEntityModificationUpdate',
 			array( $content, $title )

includes/content/ItemHandler.php
@@ -7,7 +7,7 @@ use Title;
 use Wikibase\DataModel\Entity\ItemId;
 use Wikibase\Lib\Store\EntityContentDataCodec;
 use Wikibase\Lib\Store\SiteLinkCache;
-use Wikibase\Updates\DataUpdateClosure;
+use Wikibase\Updates\DataUpdateAdapter;
 use Wikibase\Validators\EntityValidator;
 use Wikibase\Validators\ValidatorErrorLocalizer;
 
@@ -114,7 +114,7 @@ class ItemHandler extends EntityHandler {
 	public function getEntityDeletionUpdates( EntityContent $content, Title $title ) {
 		$updates = array();
 
-		$updates[] = new DataUpdateClosure(
+		$updates[] = new DataUpdateAdapter(
 			array( $this->siteLinkStore, 'deleteLinksOfItem' ),
 			$content->getEntityId()
 		);
@@ -141,12 +141,12 @@ class ItemHandler extends EntityHandler {
 		$updates = array();
 
 		if ( $content->isRedirect() ) {
-			$updates[] = new DataUpdateClosure(
+			$updates[] = new DataUpdateAdapter(
 				array( $this->siteLinkStore, 'deleteLinksOfItem' ),
 				$content->getEntityId()
 			);
 		} else {
-			$updates[] = new DataUpdateClosure(
+			$updates[] = new DataUpdateAdapter(
 				array( $this->siteLinkStore, 'saveLinksOfItem' ),
 				$content->getEntity()
 			);

includes/content/PropertyHandler.php
@@ -6,7 +6,7 @@ use DataUpdate;
 use Title;
 use Wikibase\DataModel\Entity\PropertyId;
 use Wikibase\Lib\Store\EntityContentDataCodec;
-use Wikibase\Updates\DataUpdateClosure;
+use Wikibase\Updates\DataUpdateAdapter;
 use Wikibase\Validators\EntityValidator;
 use Wikibase\Validators\ValidatorErrorLocalizer;
 
@@ -133,7 +133,7 @@ class PropertyHandler extends EntityHandler {
 	public function getEntityDeletionUpdates( EntityContent $content, Title $title ) {
 		$updates = array();
 
-		$updates[] = new DataUpdateClosure(
+		$updates[] = new DataUpdateAdapter(
 			array( $this->infoStore, 'removePropertyInfo' ),
 			$content->getEntity()->getId()
 		);
@@ -166,7 +166,7 @@ class PropertyHandler extends EntityHandler {
 			PropertyInfoStore::KEY_DATA_TYPE => $content->getProperty()->getDataTypeId()
 		);
 
-		$updates[] = new DataUpdateClosure(
+		$updates[] = new DataUpdateAdapter(
 			array( $this->infoStore, 'setPropertyInfo' ),
 			$content->getEntity()->getId(),
 			$info