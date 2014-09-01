wikimedia/mediawiki-extensions-Wikibase

Eliminated PropertyEditTool
Change-Id: I8ad97a4ecb50f11a9be8447500bf0a300ca8d4e7

repo/resources/wikibase.ui.entityViewInit.js
@@ -236,7 +236,7 @@
 			}
 		} );
 
-		wb.ui.initTermBox( entity, repoApi );
+		wb.initTermBox( entity, repoApi );
 	}
 
 	function registerEditRestrictionHandlers() {

wmde/WikibaseRepository

resources/wikibase.ui.entityViewInit.js
@@ -245,7 +245,7 @@
 			}
 		} );
 
-		wb.ui.initTermBox( entity, abstractedRepoApi );
+		wb.initTermBox( entity, abstractedRepoApi );
 	}
 
 	function registerEditRestrictionHandlers() {