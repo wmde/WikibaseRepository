wikimedia/mediawiki-extensions-Wikibase

Catch exceptions on SpecialSetSiteLink
Bug: 69536
Change-Id: I540958a719cc0ad03d04917424482a7b35664262

repo/includes/specials/SpecialSetSiteLink.php
@@ -2,6 +2,7 @@
 
 namespace Wikibase\Repo\Specials;
 
+use Exception;
 use Html;
 use OutOfBoundsException;
 use Status;
@@ -316,6 +317,10 @@ class SpecialSetSiteLink extends SpecialModifyEntity {
 			$name = 'badge-' . $badgeId;
 			$title = $this->getTitleForBadge( new ItemId( $badgeId ) );
 
+			if ( $title === null ) {
+				continue;
+			}
+
 			$options .= Html::rawElement(
 				'div',
 				array(
@@ -346,17 +351,22 @@ class SpecialSetSiteLink extends SpecialModifyEntity {
 	 * @todo use TermLookup when we have one
 	 *
 	 * @param EntityId $badgeId
-	 * @return string
+	 * @return string|null
 	 */
 	private function getTitleForBadge( EntityId $badgeId ) {
-		$entity = $this->loadEntity( $badgeId )->getEntity();
-		$languageCode = $this->getLanguage()->getCode();
+		try {
+			$entity = $this->loadEntity( $badgeId )->getEntity();
+			$languageCode = $this->getLanguage()->getCode();
 
-		$labels = $entity->getFingerprint()->getLabels();
-		if ( $labels->hasTermForLanguage( $languageCode ) ) {
-			return $labels->getByLanguage( $languageCode )->getText();
-		} else {
-			return $badgeId->getSerialization();
+			$labels = $entity->getFingerprint()->getLabels();
+			if ( $labels->hasTermForLanguage( $languageCode ) ) {
+				return $labels->getByLanguage( $languageCode )->getText();
+			} else {
+				return $badgeId->getSerialization();
+			}
+		} catch ( Exception $ex ) {
+			wfWarn( 'Error fetching title for badge: ' . $ex->getMessage() );
+			return null;
 		}
 	}
 

wmde/WikibaseRepository

includes/specials/SpecialSetSiteLink.php
@@ -5,6 +5,7 @@ namespace Wikibase\Repo\Specials;
 use Html;
 use OutOfBoundsException;
 use Status;
+use UserInputException;
 use Wikibase\ChangeOp\ChangeOpException;
 use Wikibase\ChangeOp\SiteLinkChangeOpFactory;
 use Wikibase\CopyrightMessageBuilder;
@@ -316,6 +317,10 @@ class SpecialSetSiteLink extends SpecialModifyEntity {
 			$name = 'badge-' . $badgeId;
 			$title = $this->getTitleForBadge( new ItemId( $badgeId ) );
 
+			if ( $title === null ) {
+				continue;
+			}
+
 			$options .= Html::rawElement(
 				'div',
 				array(
@@ -346,17 +351,23 @@ class SpecialSetSiteLink extends SpecialModifyEntity {
 	 * @todo use TermLookup when we have one
 	 *
 	 * @param EntityId $badgeId
-	 * @return string
+	 * @return string|null
 	 */
 	private function getTitleForBadge( EntityId $badgeId ) {
-		$entity = $this->loadEntity( $badgeId )->getEntity();
-		$languageCode = $this->getLanguage()->getCode();
+		try {
+			$entity = $this->loadEntity( $badgeId )->getEntity();
+			$languageCode = $this->getLanguage()->getCode();
 
-		$labels = $entity->getFingerprint()->getLabels();
-		if ( $labels->hasTermForLanguage( $languageCode ) ) {
-			return $labels->getByLanguage( $languageCode )->getText();
-		} else {
-			return $badgeId->getSerialization();
+			$labels = $entity->getFingerprint()->getLabels();
+			if ( $labels->hasTermForLanguage( $languageCode ) ) {
+				return $labels->getByLanguage( $languageCode )->getText();
+			} else {
+				return $badgeId->getSerialization();
+			}
+		} catch ( UserInputException $ex ) {
+			// log a warning because this indicates a wrong configuration
+			wfLogWarning( 'Error fetching title for badge: ' . $ex->getMessage() );
+			return null;
 		}
 	}
 