fc1552bcaa67fe61ed8bb86f248ae6958bee2f12
jeroendedauw <jeroendedauw@gmail.com>
2014-10-19 09:41:09 +0200

    Update test bootstrap

--- tests/bootstrap.php
+++ tests/bootstrap.php
@@ -7,8 +7,10 @@ if ( PHP_SAPI !== 'cli' ) {
 error_reporting( E_ALL | E_STRICT );
 ini_set( 'display_errors', 1 );
 
-if ( !is_readable( __DIR__ . '/../vendor/autoload.php' ) ) {
-	die( 'You need to install this package with Composer before you can run the tests' );
-}
+if ( !defined( 'WIKIBASE_DATAMODEL_VERSION' ) ) {
+	if ( !is_readable( __DIR__ . '/../vendor/autoload.php' ) ) {
+		die( 'You need to install this package with Composer before you can run the tests' );
+	}
 
-require_once( __DIR__ . '/../vendor/autoload.php' );
+	require_once( __DIR__ . '/../vendor/autoload.php' );
+}
